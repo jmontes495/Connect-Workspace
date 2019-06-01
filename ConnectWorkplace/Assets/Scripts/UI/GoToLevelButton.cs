d45f8af7cafa007c26b81174a3f8c9b3fcb5a2a7
Nicolas Trillos <ntrillos@jamcity.com>
2019-06-01 06:44:16 -0500

    Added start scene and go to level buttons. Reorganized the project into folders because I'm like that.

---
+++ ConnectWorkplace/Assets/Scripts/UI/GoToLevelButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+[RequireComponent(typeof(Button))]
+public class GoToLevelButton : MonoBehaviour
+{
+    [SerializeField]
+    private LevelName level = LevelName.OfficeGrid;
+
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(GoToLevel);
+    }
+
+    private void GoToLevel()
+    {
+        if (Application.CanStreamedLevelBeLoaded("" + level))
+            SceneManager.LoadScene("" + level);
+        else
+            Debug.LogError("Had an issue loading " + level.ToString());
+    }
+}
---
+++ ConnectWorkplace/Assets/Scripts/UI/LevelName.cs
@@ -0,0 +1,9 @@
+﻿public enum LevelName
+{
+    OfficeGrid = 0,
+    Level1 = 1,
+    Level2 = 2,
+    Level3 = 3,
+    Level4 = 4,
+    Level5 = 5,
+}
