b14bde4f0aa590bd762822a77571d959c0532935
Juliana Montes <jmontes@jamcity.com>
2019-06-01 23:01:31 -0500

    Cleaned up the prefabs to make life easier

---
+++ ConnectWorkplace/Assets/Scripts/Gameplay/GridManipulation/ObjectColorizer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColorizer : MonoBehaviour
+{
+    private Renderer renderer;
+
+    [SerializeField]
+    private Color theColor;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = theColor;
+    }
+
+    private void Update()
+    {
+        if (renderer.material.color != theColor)
+        {
+            renderer = GetComponent<Renderer>();
+            renderer.material.color = theColor;
+        }
+    }
+
+}
