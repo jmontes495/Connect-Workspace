a151556d107aa2648df139408ae4cb13feb24b6a
Juliana Montes <jmontes@jamcity.com>
2019-06-02 19:01:07 -0500

    Added the sound sensible

---
+++ ConnectWorkplace/Assets/Scripts/Gameplay/Characters/PersonalityTraits/SoundSensible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSensible : PersonalTrait
+{
+    private void Start()
+    {
+        traitType = PersonalityTrait.SoundSensible;
+    }
+
+    public override TypesOfReaction AffectOther(PersonalTrait affectee, GridPosition theirPosition)
+    {
+        return TypesOfReaction.None;
+    }
+
+    protected override bool CheckIfAffectingPosition(GridPosition theirPosition)
+    {
+        return false;
+    }
+}
