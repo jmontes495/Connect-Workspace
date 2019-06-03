00f853b1f4e3ae6aa8efdbecc03f4f47a5d71ad8
David <david.suarez@brainz.co>
2019-06-03 15:58:25 -0500

    Characters animations imported.

---
+++ ConnectWorkplace/Assets/Scripts/Configs/CharactersAnimationConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct CharacterAnimationsData
+{
+    public string personality;
+    public TypeOfPersonality typePersonality;
+    public int animationId;
+}
+
+public class CharactersAnimationConfigurations: ScriptableObject
+{
+    [SerializeField]
+    private CharacterAnimationsData[] characterAnimations;
+
+    public CharacterAnimationsData[] CharacterAnimations
+    {
+        get { return characterAnimations; }
+    }
+} 
