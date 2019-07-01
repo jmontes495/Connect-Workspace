f0fe73983f83af6cc3e43abc993b00068921210c
Juliana Montes <jmontes@jamcity.com>
2019-07-01 08:38:26 -0500

    Created exit game functionality

---
+++ ConnectWorkplace/Assets/GameQuitter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameQuitter : MonoBehaviour
+{
+    [SerializeField]
+    private Button exitButton;
+
+    [SerializeField]
+    private GameObject screen;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        exitButton.onClick.AddListener(ExitGame);
+        screen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            screen.SetActive(!screen.activeInHierarchy);
+        }
+    }
+
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
